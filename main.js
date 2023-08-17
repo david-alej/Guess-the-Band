06ca6b480a8a4f77308229e34704dbbbc09587a9
David <david12777@outlook.com>
2023-08-17 17:11:57 -0600

    started working more on main code

--- main.js
+++ main.js
@@ -2,20 +2,69 @@ import { bands } from "./singerData.js"
 
 let startGameButton = document.getElementById("start-game")
 let startGameButtonDivision = document.getElementById("container")
-let quote = document.getElementById("quote")
+let lyrics = document.getElementById("lyrics")
 let options = document.getElementById("container-1")
 
-let singersName = document.getElementsByClassName("singer")
-let singersImage = document.getElementsByClassName("singer-image")
+let bandsName = document.getElementsByClassName("bands")
+let bandsImage = document.getElementsByClassName("bands-image")
+
+// Helper function
+function randomNumber(upperLimit) {
+  return Math.floor(Math.random() * upperLimit)
+}
+
+function removeElement(array, unwantedElement) {
+  // error here: array.indexOf() is not a function: Because it is used on a string.
+  array.indexOf(unwantedElement)
+  array.splice(unwantedElement, 1)
+}
+
+function fillGameScreen() {
+  const optionsArray = [0, 1, 2]
+  const answerOption = randomNumber(3)
+  removeElement(optionsArray, answerOption)
+  const secondOption = optionsArray[randomNumber(2)]
+  removeElement(optionsArray, secondOption)
+  const thirdOption = optionsArray[0]
+  const answer = bands[randomNumber(bands.length)]
+
+  bandsName[answerOption].innerHTML = answer.name
+  bandsImage[answerOption].src = answer.image
+  lyrics.innerHTML = answer.lyricsSongs[randomNumber(answer.lyricsSongs.length)]
+
+  if (answer.lyricsSongs.length == 1) {
+    removeElement(bands, answer)
+  } else {
+    removeElement(answer.lyricsSongs, lyrics.innerHTML)
+  }
+
+  let bandsIndexes = Array.from(Array(bands.length).keys())
+  let bandIndex = randomNumber(bands.length)
+  removeElement(bandIndex)
+  let band = bands[bandIndex]
+  bandsName[secondOption].innerHTML = band.name
+  bandsImage[secondOption].src = band.image
+
+  bandIndex = bandsIndexes[randomNumber(bandsIndexes.length)]
+  band = bands[bandIndex]
+  bandsName[thirdOption].innerHTML = band.name
+  bandsImage[thirdOption].src = band.image
+}
 
 function startGame() {
   startGameButtonDivision.style.display = "none"
-  quote.style.display = "block"
+  lyrics.style.display = "block"
   options.style.display = "block"
-  const answer = bands[Math.floor(Math.random() * bands.length)]
-  const answerOption = Math.floor(Math.random() * 3)
-  singersName[answerOption].innerHTML = answer.name
-  singersImage[answerOption].src = answer.image
+  // const answer = bands[randomNumber(bands.length)]
+  // const answerOption = randomNumber(3)
+  // const answerLyric =
+  //   answer.lyricsSongs[randomNumber(answer.lyricsSongs.length)][0]
+
+  // singersName[answerOption].innerHTML = answer.name
+  // console.log(answer.image, answer.name)
+  // singersImage[answerOption].src = answer.image
+  // lyrics.innerHTML = answerLyric
+  fillGameScreen()
 }
 
 startGameButton.onclick = startGame
--- singerData.js
+++ singerData.js
@@ -1,10 +1,10 @@
-function bandFactory(name, songsQoutes) {
+function bandFactory(name, lyricsSongs) {
   return {
     name,
-    songsQoutes,
+    lyricsSongs,
     get image() {
       let imageName = this.name.toLowerCase().split(" ").join("-")
-      return "./" + imageName + ".jpeg"
+      return "./images/" + imageName + ".jpeg"
     },
   }
 }
@@ -166,3 +166,161 @@ const bands = [
 ]
 
 export { bands }
+
+// const bands = [
+//   bandFactory("System of a Down", [
+//     [
+//       "Wake up (wake up) grab a brush and put a little make-up Hide the scars to fade away the (hide the scars to fade away the shake-up)",
+//       "Chop Suey!",
+//     ],
+//     [
+//       "La,la,la,la,la,la,la,la,la,la,ooh-ooh Everybody's going to the party, have a real good time Dancing in the desert, blowing up the sunshine",
+//       "B.Y.O.B",
+//     ],
+//   ]),
+//   bandFactory("Slipknot", [
+//     [
+//       "I push my fingers into my eyes It's the only thing that slowly stops the ache But it's made of all the things I have to take",
+//       "Duality",
+//     ],
+//   ]),
+//   bandFactory("Rise Against", [
+//     [
+//       "I don't hate you boy I just want to save while there's still something left to save.",
+//       "Savior",
+//     ],
+//   ]),
+//   bandFactory("Red Hot Chili Peppers", [
+//     [
+//       "California, rest in peace simultaneous release California, show your teeth",
+//       "Dani California",
+//     ],
+//   ]),
+//   bandFactory("Evanescence", [
+//     [
+//       "Wake me up inside (save me) Call my name and save me from the dark (wake me up)",
+//       "Bring Me To Life",
+//     ],
+//   ]),
+//   bandFactory("Linkin Park", [
+//     [
+//       "I tried so hard and got so far But in the end, it doesn't even matter",
+//       "In the End",
+//     ],
+//     [
+//       "I've become so numb I can't feel you there Become so tired So much more aware",
+//       "Numb",
+//     ],
+//     [
+//       "Crawling in my skin These wounds, they will not heal Fear is how I fall",
+//       "Crawling",
+//     ],
+//   ]),
+//   bandFactory("Paramore", [
+//     [
+//       "They taped over your mouth Scribbled out the truth with their lies Your little Spies",
+//       "crushcrushcrush",
+//     ],
+//   ]),
+//   bandFactory("Breaking Benjamin", [
+//     [
+//       "Show me how it ends, it's alright Show me how denfenseless you really are Satisfied and empty inside",
+//       "So Cold",
+//     ],
+//   ]),
+//   bandFactory("Incubus", [
+//     [
+//       "Whatever tommorrow brings, I'll be there With open arms and eyes, yeah Whatever tommorrow brings, I'll be there",
+//       "Drive",
+//     ],
+//   ]),
+//   bandFactory("Trapt", [
+//     [
+//       "Back off, I'll take you on Headstrong to take on any one I know that you are wrong",
+//       "Headstrong",
+//     ],
+//   ]),
+//   bandFactory("Limp Bizkit", [
+//     [
+//       "Now move in, now move out, hands up, nows hands down Back up, back up, tell me what ya gonna do now Breathe in, now Breathe in, now breathe out, hands up, now hands down",
+//       "Rollin'",
+//     ],
+//     [
+//       "It's all about the he says, she says bullshit I think you better quit, let the shit slip Or you'll be leaving with a fat lip",
+//       "Break Stuff",
+//     ],
+//   ]),
+//   bandFactory("Skillet", [
+//     [
+//       "I feel it deep within, it's just beneath the skin I must confess that I feel like a monster I hate what I've become, the nightmare's just begun",
+//       "Monster",
+//     ],
+//   ]),
+//   bandFactory("Foo Fighters", [
+//     [
+//       "What if I say I'm not like the others? What if I say I'm not just another one of your plays? You're the pretender",
+//       "The Pretender",
+//     ],
+//     [
+//       "Is something getting the best, the best, the best The best of you? Is someone getting the best, the best, the best",
+//       "Best of You",
+//     ],
+//   ]),
+//   bandFactory("Three Days Grace", [
+//     [
+//       "So what if you can see the darkest side of me? No one would ever change this animal I have become Help me believe it's not the real me",
+//       "Animal I Have become",
+//     ],
+//   ]),
+//   bandFactory("Stone Sour", [
+//     [
+//       "I'm looking at you through the glass Don't know how much time has passed Oh God, it feels like forever",
+//       "Through Glass",
+//     ],
+//   ]),
+//   bandFactory("Disturbed", [
+//     [
+//       "If this disturbs you then walk way You will remember the night you were struck by the sight of Ten thousand fists in the air",
+//       "Ten Thousands Fists",
+//     ],
+//   ]),
+//   bandFactory("Papa Roach", [
+//     [
+//       "Cut my life into pieces I've reached my last resort Suffocation, no breathing",
+//       "Last Resort",
+//     ],
+//   ]),
+//   bandFactory("Thirty Seconds To Mars", [
+//     ["Come, break me down Bury me, bury me I am finished with you", "The Kill"],
+//   ]),
+//   bandFactory("Audioslave", [
+//     [
+//       "In your house, I long to be Room by room, patiently I'll wait for you there Like a Stone",
+//       "Like a Stone",
+//     ],
+//   ]),
+//   bandFactory("3 Doors Down", [
+//     [
+//       "If I go crazy, then will you still call me Superman? If I'm alive and well, will you be there and holding my hand? I'll keep you by my side with my superhuman might Kryptonite",
+//       "Kryptonite",
+//     ],
+//     [
+//       "So hold me when I'm here, right me when I'm wrong Hold me when I'm scared and love me when I'm gone Everything I am and everything in me",
+//       "When I'm gone",
+//     ],
+//   ]),
+//   bandFactory("Static-X", [
+//     [
+//       "You're trying to take me You're trying to make me This is the only Give me the only thing",
+//       "The Only",
+//     ],
+//   ]),
+//   bandFactory("Drowning Pool", [
+//     [
+//       "Let the bodies hit the floor Let the bodies hit the floor Let the bodies hit the floor",
+//       "Bodies",
+//     ],
+//   ]),
+// ]
+
+// export { bands }
