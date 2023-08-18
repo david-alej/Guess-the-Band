58f5e3e6b42416597e5cb5c6a7b85932f46f6ab5
David <david12777@outlook.com>
2023-08-18 10:45:35 -0600

    More progress on the main code

--- main.js
+++ main.js
@@ -10,9 +10,10 @@ let bandsName = document.getElementsByClassName("bands")
 let bandsImage = document.getElementsByClassName("bands-image")
 
 // Initializing meta information about the game
-let round = 1
+let round = 0
 let games = 0
 let answers = []
+let userChoices = []
 let bandsCache = []
 
 // Following two functions are Helper functions
@@ -25,8 +26,37 @@ function removeElement(array, unwantedElement) {
   array.splice(index, 1)
 }
 
-function fillGameScreen() {
-  if (round > 15) {
+function fillGameScreen(event) {
+  // Stores user choices of options on userChoices array
+  if (round !== 0 && round < 11) {
+    if (
+      event.target === boxes[0] ||
+      event.target === bandsName[0] ||
+      event.target === bandsImage[0]
+    ) {
+      userChoices.push(0)
+    } else if (
+      event.target === boxes[1] ||
+      event.target === bandsName[1] ||
+      event.target === bandsImage[1]
+    ) {
+      userChoices.push(1)
+    } else {
+      userChoices.push(2)
+    }
+  } else if (round === 0) {
+    round++
+  }
+
+  if (round > 10) {
+    // console.log(answers)
+    console.log(userChoices)
+    round = 0
+    games++
+    if (games === 2) {
+      console.log("Game Finished")
+      return
+    }
     return
   }
   // Getting the positions of the band answer for the lyric and the other two options randomly
@@ -53,7 +83,8 @@ function fillGameScreen() {
   const answerIndex = bandsIndexes[randomNumber(bandsIndexes.length)]
   const answer = bands[answerIndex]
   const lyricSongIndex = randomNumber(answer.lyricsSongs.length)
-  answers.push([answer.name, answer.lyricsSongs[lyricSongIndex]])
+  answers.push([answer.name, answer.lyricsSongs[lyricSongIndex]], answerOption)
+  removeElement(bandsIndexes, bands.indexOf(answer))
 
   // filling out the lyric and band answer on the game screen
   bandsName[answerOption].innerHTML = answer.name
@@ -61,17 +92,15 @@ function fillGameScreen() {
   lyrics.innerHTML = answer.lyricsSongs[lyricSongIndex][0]
   bandsCache.push(answer)
 
-  //Removing the band answer lyric or the whole band from the bands array
-  if (answer.lyricsSongs.length === 1) {
-    removeElement(bands, answer)
-  } else {
-    removeElement(answer.lyricsSongs, answer.lyricsSongs[lyricSongIndex])
-  }
+  // //Removing the band answer lyric or the whole band from the bands array
+  // if (answer.lyricsSongs.length === 1) {
+  //   removeElement(bandsIndexes, bands.indexOf(answer))
+  //   removeElement(bands, answer)
+  // } else {
+  //   removeElement(answer.lyricsSongs, answer.lyricsSongs[lyricSongIndex])
+  // }
 
   // filling out an option value with a random band and making sure that this option value and the remaining option value are not the same.
-  if (answer in bands) {
-    removeElement(bandsIndexes, bands.indexOf(answer))
-  }
   let bandIndex = randomNumber(bandsIndexes.length)
   removeElement(bandsIndexes, bandIndex)
   let band = bands[bandIndex]
@@ -85,6 +114,7 @@ function fillGameScreen() {
   bandsName[thirdOption].innerHTML = band.name
   bandsImage[thirdOption].src = band.image
   bandsCache.push(band)
+  // console.log(bandsCache)
 
   // +1 to the total rounds played
   round++
@@ -100,6 +130,6 @@ function startGame() {
 
 startGameButton.onclick = startGame
 for (let i = 0; i < 3; i++) {
-  boxes[i].onclick = fillGameScreen
+  boxes[i].addEventListener("click", fillGameScreen)
 }
 console.log("abcdefghijk"[Math.floor(Math.random() * 11)])
